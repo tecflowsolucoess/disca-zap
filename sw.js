51413f869b8478e06960b11c8cf988553e7b33c7
tecflowsolucoess <tecflowsolucoess@gmail.com>
2026-06-30 16:44:12 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,60 @@
-const CACHE_NAME = "discazap-v1";
+const CACHE_NAME = "discazap-v2";
 
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json"
+const ASSETS = [
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./icon-192.png",
+    "./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+
+    self.skipWaiting();
+
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(ASSETS))
+    );
+
+});
+
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+
+        caches.keys().then(keys => {
+
+            return Promise.all(
+
+                keys.map(key => {
+
+                    if (key !== CACHE_NAME) {
+
+                        return caches.delete(key);
+
+                    }
+
+                })
+
+            );
+
+        }).then(() => self.clients.claim())
+
+    );
+
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+
+    if (event.request.method !== "GET") return;
+
+    event.respondWith(
+
+        caches.match(event.request)
+
+            .then(response => response || fetch(event.request))
+
+    );
+
 });
